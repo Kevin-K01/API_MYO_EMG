88c4899ace7503067b3bef474f060342a39c8319
kevin <kevin009853@gmail.com>
2025-03-10 20:10:45 -0600

    se modificó la forma de guardar los datos emg

--- API_MYO.py
+++ API_MYO.py
@@ -32,7 +32,7 @@ def add_patient():
     if not all(k in data for k in ["nombre", "curp", "Extremidad_Afectada", "observaciones"]):
         return {"error": "Datos incompletos"}, 400
 
-    patient_file = os.path.join(DATA_DIR, f"{data['curp']}")
+    patient_file = os.path.join(DATA_DIR, f"{data['nombre']}_{data['curp']}")
     patient_exists = False
 
     # Comprobamos si el paciente ya existe en el archivo CSV
@@ -51,8 +51,6 @@ def add_patient():
     # Asignar el directorio del paciente al collector
     EmgCollector.current_patient_file = patient_file  # Asignamos la carpeta a la clase
 
-
-
     # Agregar la información del paciente al archivo principal de pacientes
     with open(CSV_FILE, mode="a", newline="") as file:
         writer = csv.writer(file)
@@ -69,8 +67,8 @@ class EmgCollector(myo.DeviceListener):
         self.emg_data_queue = deque(maxlen=n)
         self.selected_sensors = [True] * 8
         self.is_recording = False
-        self.emg_buffer = []
-        self.session_data = []
+        self.session_saved = False  # Controla si la sesión ya fue guardada
+        self.session_file = None
         self.current_patient_file = None
         self.session_number = None  # Para almacenar el número de sesión
         self.observations = None  # Para almacenar las observaciones
@@ -85,36 +83,43 @@ class EmgCollector(myo.DeviceListener):
             self.emg_data_queue.append((event.timestamp, filtered_emg))
             socketio.emit('emg_data', {'timestamp': event.timestamp, 'emg': filtered_emg})
 
-            if self.is_recording:
-                if self.session_number is not None and self.observations is not None:
-                    # Aquí agregamos la sesión y observaciones a los datos EMG
-                    self.emg_buffer.append((self.session_number, self.observations, event.timestamp, filtered_emg))
-                    print(f"Datos añadidos al buffer: {self.emg_buffer[-1]}")  # Verifica si los datos se están agregando
+            # Si estamos grabando, guardamos los datos directamente en el archivo CSV
+            if self.is_recording and self.current_patient_file:
+                # Guardar los datos EMG en el archivo correspondiente
+                self.save_data(event.timestamp, filtered_emg)
 
-    def save_data(self):
-        if self.current_patient_file and self.emg_buffer:
+                if not self.session_saved:
+                    self.save_session()
+
+    def save_session(self):
+        # Guardar los datos de sesión y observaciones (si no existen aún los encabezados)
+        if not os.path.exists(self.session_file):
+            with open(self.session_file, mode="a", newline="", encoding="utf-8") as file:
+                writer = csv.writer(file)
+
+                # Escribir encabezados en el archivo de sesiones solo una vez
+                writer.writerow(["sesion", "observaciones"])
+                print("Escribiendo encabezados de sesiones...")
+
+        # Siempre agregar una nueva sesión con sus observaciones
+        if not self.session_saved:
+            self.session_saved = True
+            with open(self.session_file, mode="a", newline="", encoding="utf-8") as file:
+                writer = csv.writer(file)
+                writer.writerow([self.session_number, self.observations])
+
+
+    def save_data(self, timestamp, filtered_emg):
+        if self.current_patient_file:
             # Extraer el nombre del paciente de la ruta del archivo
             patient_name = os.path.splitext(os.path.basename(self.current_patient_file))[0]
+            patient_curp = os.path.splitext(os.path.basename(self.current_patient_file))[1]
             patient_directory = os.path.join("pacientes", patient_name)
             os.makedirs(patient_directory, exist_ok=True)
 
             # Archivos separados por paciente
-            session_file = os.path.join(patient_directory, f"{patient_name}_sesiones.csv")
-            emg_file = os.path.join(patient_directory, f"{patient_name}_datos_emg.csv")
-
-            # Guardar los datos de sesión y observaciones (si no existen aún los encabezados)
-            if not os.path.exists(session_file):
-                with open(session_file, mode="a", newline="", encoding="utf-8") as file:
-                    writer = csv.writer(file)
-
-                    # Escribir encabezados en el archivo de sesiones solo una vez
-                    writer.writerow(["sesion", "observaciones"])
-                    print("Escribiendo encabezados de sesiones...")
-
-            # Siempre agregar una nueva sesión con sus observaciones
-            with open(session_file, mode="a", newline="", encoding="utf-8") as file:
-                writer = csv.writer(file)
-                writer.writerow([self.session_number, self.observations])
+            self.session_file = os.path.join(patient_directory, f"sesiones.csv")
+            emg_file = os.path.join(patient_directory, f"{self.session_number}_emg.csv")
 
             # Guardar datos EMG
             if not os.path.exists(emg_file):
@@ -131,23 +136,19 @@ class EmgCollector(myo.DeviceListener):
             # Guardar los datos EMG (sensores 1-8) cada vez que se capture
             with open(emg_file, mode="a", newline="", encoding="utf-8") as file:
                 writer = csv.writer(file)
+                writer.writerow([timestamp] + filtered_emg)  # Guardar timestamp + datos EMG
+                print(f"Guardando fila de datos EMG: {[timestamp] + filtered_emg}")
 
-                # Guardar los datos EMG (sensores 1-8) con su timestamp
-                for row in self.emg_buffer:
-                    emg_data = row[3]  # Datos EMG (sensores 1-8)
-                    timestamp = row[2]  # Timestamp
-                    writer.writerow([timestamp] + emg_data)  # Guardar timestamp + datos EMG
-                    print(f"Guardando fila de datos EMG: {[timestamp] + emg_data}")
-
-            # Limpiar el buffer de datos EMG después de guardar
-            self.emg_buffer = []
-            print("Datos guardados y buffer limpiado.")
-
+    def stop_recording(self):
+        """ Detiene la grabación y guarda los datos restantes """
+        self.is_recording = False
+        self.session_saved = False  # Permitir guardar una nueva sesión en futuras grabaciones
+        print("Grabación detenida")
 
 @app.route("/start_emg_capture", methods=["POST"])
 def start_emg_capture():
     data = request.json
-    required_keys = ["nombre", "sesion", "observaciones"]
+    required_keys = ["nombre", "sesion", "curp", "observaciones"]
     if not all(key in data for key in required_keys):
         return {"error": f"Faltan datos: {', '.join([key for key in required_keys if key not in data])}"}, 400
 
@@ -157,14 +158,15 @@ def start_emg_capture():
         reader = csv.DictReader(file)
         patient_found = False  # Variable para saber si encontramos el paciente
         for row in reader:
-            if row["nombre"].strip().upper() == data["nombre"].strip().upper():
+            if row["curp"].strip().upper() == data["curp"].strip().upper():
                 patient_curp = row["curp"]
-                patient_file = os.path.join(DATA_DIR, f"{patient_curp}")
+                Nombre = data["nombre"]
+                patient_file = os.path.join(DATA_DIR, f"{Nombre}_{patient_curp}")
                 patient_found = True
                 break
 
         if not patient_found:
-            return {"error": f"Paciente con nombre '{data['nombre']}' no encontrado en el registro."}, 400
+            return {"error": f"Paciente con nombre '{data['nombre']}' con curp: {data['curp']} no encontrado en el registro."}, 400
 
     rec_emg.listener.is_recording = True
     rec_emg.listener.current_patient_file = patient_file
@@ -177,9 +179,9 @@ def start_emg_capture():
 @app.route("/stop_emg_capture", methods=["POST"])
 def stop_emg_capture():
     if rec_emg.listener.is_recording:
-        rec_emg.listener.is_recording = False
-        rec_emg.listener.save_data()
+        rec_emg.listener.stop_recording() #detener la captura de datos
         rec_emg.listener.current_patient_file = None
+
         print("Captura de EMG detenida y datos guardados")
 
     return {"message": "Captura de EMG detenida y datos guardados"}, 200
@@ -217,5 +219,3 @@ if __name__ == '__main__':
 
     thread_myo = Thread(target=start_myo_data_collection)
     thread_myo.start()
-
-
